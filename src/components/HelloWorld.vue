788014104dd83c8d461cd76f8d8723f6b3c7d840
luo <1968967624@qq.com>
2018-04-25 01:00:41 +0800

    aaa

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="hello">
+  罗玲玲测试代码,交换机色卡号色卡贵公司色弱顺丰到付南京东方航空俗人谁惹分别是大家发货速度和色素
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello{
+  width: 150px;
+  height: 64px; /*px*/
+  font-size: 28px; /*px*/
+  border: 1px solid #ddd; /*no*/
+}
+</style>
