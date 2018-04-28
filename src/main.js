fe04b4969ed59fea831a73e7c68bc05992f68509
罗玲玲 <luoll2@yonyou.com>
2018-04-28 17:10:40 +0800

    '配置mintUI，并修改.babelrc文件中的配置和在main.js中按需引入项目中需要的组件'

--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import 'lib-flexible'
+/*import Mint from 'mint-ui';
+Vue.use(Mint);
+import 'mint-ui/lib/style.css';*/
+/*按需引入组件，此时引入按钮*/
+import Button from 'mint-ui/lib/button';
+Vue.component(Button.name, Button);
 
 Vue.config.productionTip = false
 
